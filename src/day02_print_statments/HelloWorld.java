b0759a3b26d4c6ec15673949b2f7247a9b9dec99
Zeynab <zeynabnasibova7@gmail.com>
2022-10-09 17:33:05 -0400

    .

--- src/day02_print_statments/HelloWorld.java
+++ src/day02_print_statments/HelloWorld.java
@@ -1,7 +1,9 @@
 package day02_print_statments;
 
 public class HelloWorld {
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello word");
     }
+
 }
