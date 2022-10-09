b6c7a887206a9843da40655ba308e7979a0c9b02
Zeynab <zeynabnasibova7@gmail.com>
2022-10-09 14:48:35 -0400

    practice codingbat

---
+++ src/codingbat/Warmup1/intMax.java
@@ -0,0 +1,41 @@
+package codingbat.Warmup1;
+
+public class intMax {
+
+    public static void main(String[] args) {
+
+        /*
+Given three int values, a b c, return the largest.
+
+
+intMax(1, 2, 3) → 3
+intMax(1, 3, 2) → 3
+intMax(3, 2, 1) → 3
+
+
+         */
+
+        System.out.println(intMax(1,2,3));
+
+    }
+
+    public static int intMax(int a, int b, int c) {
+
+
+        if(a > b && a > c){
+
+            return a;
+
+        }else if(b > a && b >c){
+
+            return b;
+
+        }else{
+
+            return c;
+        }
+
+}
+
+
+}
